4677aef2b68cb824f20c6a31da22123a5b8dfb06
Evelina <evelina.venckute@swedbank.lt>
2023-06-01 11:45:30 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
   title = 'assigment';
 
-  onIntervalFired(firedNumber: number)
-{
-console.log(firedNumber);
-}}
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+}
